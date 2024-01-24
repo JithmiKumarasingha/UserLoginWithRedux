5f4d5990d3ac377c9da082221ec4bf996f1eb161
Jithmi Kumarasingha <jithmi.kumarasingha@circles.co>
2024-01-24 09:13:25 +0530

    created login and dashboard

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express =require('express');
-// const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 const cors = require('cors')
@@ -8,40 +7,15 @@ app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(express.json());
-// app.use(bodyParser.json());
 
-// app.use(cors())
 
- 
-
-const data ={
+ const data ={
    email: 'admin@hello.world',
    name: 'Jithmi Kumarasingha',
    location: 'Colombo',
    password: 'circles111'
+
 }
-
-
-// end point to get data
-// app.get('/test', (req,res) => {
-//    res.send(data);
-// })
-
-//end point to post data
-// app.post('/test/add', (req , res) => {
-//    const  { password } = req.body
-
-//    const newData ={
-//        no:data.length+1,
-//        email,
-//        password
-//    }
-
-
-//    data.push(newData)
-//    return res.json(data);
-
-// })
 
 app.post('/user-service/login', (req, res) => {
     const { email, password } = req.body;
@@ -51,19 +25,18 @@ app.post('/user-service/login', (req, res) => {
       const token = 'a_guid';
       res.json({ token });
     } else {
-      res.status(401).json({ error: `Invalid email or password ${req.body}` });
-    }
+      console.log("invalid");
+     }
 
     console.log("input taken");
   });
 
-  
-
-  app.get('/user-service', (req, res) => {
+app.get('/user-service', (req, res) => {
         res.send(data);
   });
+
+
   
- 
-  app.listen(PORT, () => {
+ app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
   }); 
